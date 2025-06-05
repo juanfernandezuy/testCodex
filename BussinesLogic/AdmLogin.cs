78299ea9190b303c521b9e1e11473d42c00647c6
juanfernandezuy <juancafernandez@bps.gub.uy>
2025-06-05 12:01:02 -0300

    Add files via upload

---
+++ BussinesLogic/AdmLogin.cs
@@ -0,0 +1,16 @@
+﻿using BussinesEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BussinesLogic
+{
+    public class AdmLogin
+    {
+        public Usuario Login(string identificador, string password)
+        {
+            return new Mappers.MapUsuarios().GetUsuarios(identificador, password)[1];
+        }
+    }
+}
---
+++ Dalcs/DalUsuarios.cs
@@ -0,0 +1,20 @@
+﻿using BussinesEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dalcs
+{
+    public class DalUsuarios
+    {
+        public List<Usuario> GetUsuarios(string identificador, string password)
+        {
+            // TO DO
+            // OPEN Usuarios.CSV
+            // Rear line by line
+            // SEARCH identificador, password that match with parameters
+            return null;
+        }
+    }
+}
