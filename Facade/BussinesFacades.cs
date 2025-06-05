5c7aa52daec9208ac9de66bdd3b94c4b7ddc2656
juanfernandezuy <juancafernandez@bps.gub.uy>
2025-06-05 12:02:01 -0300

    Add files via upload

---
+++ Facade/BussinesFacades.cs
@@ -0,0 +1,16 @@
+﻿using BussinesEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facade
+{
+    public class BussinesFacades
+    {
+        public Usuario Login(string identificador, string password)
+        {
+            return new BussinesLogic.AdmLogin().Login(identificador, password);
+        }
+    }
+}
---
+++ Mappers/MapUsuarios.cs
@@ -0,0 +1,17 @@
+﻿using BussinesEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mappers
+{
+    public class MapUsuarios
+    {
+        public List<Usuario> GetUsuarios(string identificador, string password)
+        {
+            // TODO Using dalcs project get users
+            return null;
+        }
+    }
+}
